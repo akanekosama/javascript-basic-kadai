d087aa38be0b7f327094e97c2035033e8cad386e
akanekosama <148690646+akanekosama@users.noreply.github.com>
2024-01-07 22:12:45 +0900

    Delete kadai_mdn_js/mdn.js

--- kadai_mdn_js/mdn.js
+++
@@ -1,9 +0,0 @@
-const date = new Date(2024,(11-1),22);
-
-const [month, day, year] = [
-  date.getMonth(11),
-  date.getDate(22),
-  date.getFullYear(2024),
-];
-
-console.log(date.toLocaleString("ja-JP"));
